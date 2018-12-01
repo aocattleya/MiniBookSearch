f5901a3da153fe5f6ed7774dcb8740678a575d19
aocattleya <aocattleya@yahoo.co.jp>
2018-12-01 22:50:37 +0900

    (重要)openBD用に大部分を修正

--- static/main.js
+++ static/main.js
@@ -1,5 +1,5 @@
 // -*- coding: utf-8; -*-
-Vue.component( 'detail-modal', {
+Vue.component('detail-modal', {
     template: '#detail-modal',
     props: {
         index: Number,
@@ -8,7 +8,7 @@ Vue.component( 'detail-modal', {
     data: function () {
         return this.results[this.index];
     }
-} );
+});
 
 
 var app = new Vue({
@@ -20,50 +20,50 @@ var app = new Vue({
 
         isbn: '',
         title: '',
-        author: '',
         publisher: '',
-        publishedDate: '',
-        description: '',
+        pubdate: '',
+        cover: '',
+        author: '',
 
-        search_clicked : 0,
+        search_clicked: 0,
 
         results: []
     },
     methods: {
         regist: function () {
-			if (this.regist_isbn.length == 0) {
-				alert('ISBNコードが入力されていません') ;
-				return ;
+            if (this.regist_isbn.length == 0) {
+                alert('ISBNコードが入力されていません');
+                return;
             }
-            
-            this.$el.querySelector("#container").style.display = "block"
-			let text = this.regist_isbn.replace(/-/g, '')
-            let url = "/regist?isbn="+text;
+            // this.$el.querySelector("#container").style.display = "block"
+            let text = this.regist_isbn.replace(/-/g, '')
+            let url = "/regist?isbn=" + text;
 
             console.log(url)
-            axios.get( url )
-                .then( response => {
-                    console.log( response );
+            axios.get(url)
+                .then(response => {
+                    console.log(response);
 
-					if (response.data.length == 0) {
-						alert('登録できませんでした（GoogleBooksに見つかりませんでした）') ;
-						return ;
-					}
-
-                    let results_old = this.results
-                    this.results = []
-                    for( let value of response.data) {
-                        this.results.push( value )
-                    }
-//					app.$forceUpdate();
-                    if ( results_old == this.results) {
-                        window.scrollTo(0,document.body.scrollHeight-this.$el.querySelector("#container").clientHeight);
-                        this.search_clicked = 0
-                    } else {
-                        this.search_clicked = 1
+                    if (response.data.length == 0) {
+                        alert('登録できませんでした（GoogleBooksに見つかりませんでした）');
+                        return;
                     }
 
-              } ) ;
+                    // let results_old = this.results
+                    // this.results = []
+                    // // for( let value of response.data) {
+                    // //     this.results.push( value )
+                    // // }
+                    // this.results.push(response.data)
+                    // //					app.$forceUpdate();
+                    // if (results_old == this.results) {
+                    //     window.scrollTo(0, document.body.scrollHeight - this.$el.querySelector("#container").clientHeight);
+                    //     this.search_clicked = 0
+                    // } else {
+                    //     this.search_clicked = 1
+                    // }
+
+                });
 
         },
 
@@ -71,64 +71,74 @@ var app = new Vue({
             // 検索option
             let search_option = [];
 
-			if (this.isbn.length != 0) {
-                search_option.push("isbn="+this.isbn);
+            if (this.isbn.length != 0) {
+                search_option.push("isbn=" + this.isbn);
             }
-			if (this.title.length != 0) {
-                search_option.push("title="+this.title);
+            if (this.title.length != 0) {
+                search_option.push("title=" + this.title);
             }
-			if (this.author.length != 0) {
-                search_option.push("author="+this.author);
+            if (this.publisher.length != 0) {
+                search_option.push("publisher=" + this.publisher);
             }
-			if (this.publisher.length != 0) {
-                search_option.push("publisher="+this.publisher);
+            if (this.pubdate.length != 0) {
+                search_option.push("pubdate=" + this.pubdate);
             }
-			if (this.publishedDate.length != 0) {
-                search_option.push("publishedDate="+this.publishedDate);
+            if (this.cover.length != 0) {
+                search_option.push("cover=" + this.cover);
             }
-			if (this.description.length != 0) {
-                search_option.push("description="+this.description);
+            if (this.author.length != 0) {
+                search_option.push("author=" + this.author);
             }
 
-            if ( search_option.length == 0 ) {
+            if (search_option.length == 0) {
                 // 採用カウントが 0 の場合:
-				alert('検索条件を設定してください') ;
-				return ;
+                alert('検索条件を設定してください');
+                return;
             }
+            console.log("aaaa");
 
             let search_str = search_option.join("&");
-            this.$el.querySelector("#container").style.display = "block"
+            // this.$el.querySelector("#container").style.display = "block"
 
-            let url = "/search?"+search_str;
-			let url2 = url.replace(/#/g, '%23').replace(/\+/g, '%2b');
+            let url = "/search?" + search_str;
+            let url2 = url.replace(/#/g, '%23').replace(/\+/g, '%2b');
             console.log(url2)
-            axios.get( url2 )
-                .then( response => {
-                    console.log( response );
+            axios.get(url2)
+                .then(response => {
+                    console.log(response);
 
-		            let results_old = this.results
-					this.results = []
-					for( let value of response.data) {
-						this.results.push( value )
-					}
-//					app.$forceUpdate();
-		            if ( results_old == this.results) {
-		                window.scrollTo(0,document.body.scrollHeight-this.$el.querySelector("#container").clientHeight);
-		                this.search_clicked = 0
-		            } else {
-		                this.search_clicked = 1
-		            }
-                } ) ;
+                    let results_old = this.results
+                    this.results = []
+                    for (let value of response.data) {
+                        this.results.push(value)
+                    }
+                    //					app.$forceUpdate();
+                    if (results_old == this.results) {
+                        if (this.$el.querySelector("#container") != null) {
+                            window.scrollTo(0, document.body.scrollHeight - this.$el.querySelector("#container").clientHeight);
+                        }
+                        this.search_clicked = 0
+                    } else {
+                        if (this.results == []) {
+                            this.search_clicked = 0
+                        }
+                        else {
+                            this.search_clicked = 1
+                        }
+                    }
+                });
         },
-        isSelect: function(num) {
+        isSelect: function (num) {
             this.isActive = num;
         }
 
     },
-    updated : function () {
+    updated: function () {
         this.$nextTick(function () {
-            if ( this.search_clicked == 1 ) {
-	            window.scrollTo(0,document.body.scrollHeight-this.$el.querySelector("#container").clientHeight);
+            if (this.search_clicked == 1) {
+                if (this.$el.querySelector("#container") != null) {
+                    window.scrollTo(0, document.body.scrollHeight - this.$el.querySelector("#container").clientHeight);
+                }
                 this.search_clicked = 0
             }
         })
